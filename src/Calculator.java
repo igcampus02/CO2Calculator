0a4f63bc836606d1e33adc8c1ec78af83a901313
irmagut <irmgard.gutmann@edu.campus02.at>
2021-10-08 19:59:04 +0200

    initial test

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a = 1;
+        System.out.println(a);
+        double b = 1.23;
+        boolean c = true;
+        char d ='X';
+        String e = ("Heute ist ein guter Tag!");
+        System.out. println (b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
